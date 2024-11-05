6abf1b84325c4441537a01176b635f09facbb24a
matusca96 <gomesloud@gmail.com>
2024-11-05 17:33:50 -0300

    chore: adjust packages versions, add eslint and prettier, remove unnecessary files

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'standard',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'prettier', 'simple-import-sort'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      { allowExpressions: true },
+    ],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'camelcase': 0
+  },
+}
--- src/ChallengeComponent.tsx
+++ src/ChallengeComponent.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-
-export function ChallengeComponent() {
+export const ChallengeComponent = (): JSX.Element => {
   return (
     <>
       {/* Delete this h2, and add your own code here. */}
-      <h2 style={{ textAlign: 'center', paddingTop: 200, paddingBottom: 200 }}>Your code goes here</h2>
+      <h2 style={{ textAlign: "center", paddingTop: 200, paddingBottom: 200 }}>
+        Your code goes here
+      </h2>
     </>
-  )
-}
+  );
+};
